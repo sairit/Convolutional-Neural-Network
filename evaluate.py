53cbf0f8ad1cb57ed2500b7377690ecdc2ca9816
Sai Yadavalli <77541149+sairit@users.noreply.github.com>
2026-06-08 11:46:58 -0400

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -6,6 +6,14 @@ import joblib
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+"""
+Notes: 
+loading a previously trained 1D CNN, preprocessing and normalizing the MIT-BIH ECG test dataset, 
+generating predictions for each heartbeat, evaluating classification performance using accuracy, 
+precision, recall, F1 score, and a confusion matrix, and visualizing the results with 
+performance metric and confusion matrix plots.
+"""
+
 # --- Load and preprocess test data ---
 test_df = pd.read_csv("Data/mitbih_test.csv", header=None)
 X_test = test_df.iloc[:, :-1].values.astype(np.float32)
